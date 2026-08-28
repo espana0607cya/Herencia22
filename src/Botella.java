1b2b8b6b2bda9071be01ed62abcc3969860e3bbe
ANDRES <espanaandres681@gmail.com>
2026-08-28 17:40:13 -0500

    Cuarto Commit, definimos método e impresion de consola

--- src/Botella.java
+++ src/Botella.java
@@ -1,5 +1,9 @@
 public class Botella {
-    String marca= " ";
+    String marca = " ";
     int CantidadDeAlco = 0;
     int TiempoDeBajarLicor = 0;
+
+    public void beber() {
+        System.out.println("La botella " + marca + " con la cantidad de licor " + CantidadDeAlco + "%" + " va hacer tomada !AHORA¡");
+    }
 }
