e5d5816b9375aca1e292bc941c88356052253f2c
ANDRES <espanaandres681@gmail.com>
2026-08-28 17:45:26 -0500

    Septimo Commit, método que no retorna y despues imprimimos

--- src/BotellaDeAguardiente.java
+++ src/BotellaDeAguardiente.java
@@ -1,5 +1,10 @@
 public class BotellaDeAguardiente extends Botella {
     int cantidadbotellas = 0;
 
+    public void mostrarDetalles() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Porcentaje de alcohol: " + CantidadDeAlco + " %");
+        System.out.println("Número de botellas: " + cantidadbotellas);
+    }
 }
 
